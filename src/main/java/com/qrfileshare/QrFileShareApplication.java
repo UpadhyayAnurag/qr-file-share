603be7e79353eebc4fd4148a4e2105e87177c09e
Anurag Upadhyay <aknurag75@gmail.com>
2026-02-27 08:15:06 +0530

    Initial Commit

---
+++ src/main/java/com/qrfileshare/QrFileShareApplication.java
@@ -0,0 +1,13 @@
+package com.qrfileshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrFileShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrFileShareApplication.class, args);
+    }
+
+}
